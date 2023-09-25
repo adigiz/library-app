00b1443852a766ad9d04294b13be3cc863919ed4
Gizwanda <adityo.gizwanda@shopee.com>
2023-09-25 10:56:23 +0700

    feat: implement admin middleware

--- src/routes/books.js
+++ src/routes/books.js
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 const booksController = require("../controllers/bookController");
 const { imageUpload } = require("../middlewares/imageUploadMiddleware");
-const authMiddleware = require("../middlewares/authMiddleware");
+const { adminMiddleware } = require("../middlewares/authMiddleware");
 
 // Books CRUD
 router.get("/books", booksController.getAllBooks);
 router.get("/books/:slug", booksController.getBookBySlug);
-router.post("/books", imageUpload.single("cover"), authMiddleware, booksController.createBook);
+router.post(
+  "/books",
+  imageUpload.single("cover"),
+  adminMiddleware,
+  booksController.createBook
+);
 router.put("/books/:id", booksController.updateBook);
 router.delete("/books/:id", booksController.deleteBook);
 
