ee788541e5eee17d7878c33b4a8c542de86f31d9
Gizwanda <adityo.gizwanda@shopee.com>
2023-11-02 17:31:11 +0700

    update sql

--- src/database/table.sql
+++ src/database/table.sql
@@ -7,10 +7,12 @@ CREATE TABLE books (
   author VARCHAR(255) NOT NULL,
   isbn VARCHAR(13) NOT NULL,
   is_borrowed BOOLEAN DEFAULT false,
-  cover TEXT,
+  imageUrl TEXT,
+  fileUrl TEXT,
   synopsis TEXT,
   published_date DATE,
   slug TEXT not null,
+  genre VARCHAR(255) NOT NULL,
   created_at TIMESTAMPTZ DEFAULT current_timestamp,
   updated_at TIMESTAMPTZ DEFAULT current_timestamp,
   deleted_at TIMESTAMPTZ
@@ -35,11 +37,79 @@ CREATE TABLE users (
   deleted_at TIMESTAMPTZ
 );
 
-INSERT INTO books (title, slug, author, isbn, is_borrowed, imageUrl, synopsis, published_date) VALUES
-  ('Harry Potter and the Philosopher Stone','harry-potter-and-the-philosopher-stone', 'JK. Rowling', '1234567890123', false, 'https://images.ctfassets.net/usf1vwtuqyxm/2DCs73x6P8seNobQ9zBSbO/1a5dfd6ed5fc0ed9545370470fc3d74c/English_Harry_Potter_1_Epub_9781781100219.jpg?w=914&q=70&fm=webp', 'Harry Potter lives with his abusive aunt and uncle, Vernon and Petunia Dursley, and their bullying son, Dudley. On Harry''s eleventh birthday, Rubeus Hagrid, a half-giant, delivers an acceptance letter from Hogwarts School of Witchcraft and Wizardry, revealing that Harry''s parents, James and Lily Potter, were wizards. When Harry was one year old, powerful dark wizard Lord Voldemort murdered his parents. Harry survived Voldemort''s killing curse that rebounded and seemingly destroyed the Dark Lord, leaving a lightning bolt-shaped scar on his forehead. Unknown to Harry, this act made him famous in the wizarding world.\n Hagrid escorts Harry to Diagon Alley, the hidden wizard commerce and retail section in London. Harry''s parents have left him a fortune kept in Gringotts Wizarding Bank, which he uses to buy school supplies and a wand from Ollivander''s. The cores of Harry and Lord Voldemort''s wands have feathers from the same phoenix bird, making them "brothers". Hagrid gifts Harry an owl as a birthday present. Harry names her Hedwig. A month later, Harry boards the Hogwarts Express at King''s Cross railway station''s secret Platform 9+3⁄4. En route to Hogwarts, Harry befriends fellow first year Ron Weasley and meets Hermione Granger, whom the two boys initially dislike.\n Harry runs afoul of Draco Malfoy, the spoiled son of a prominent wizard. At Hogwarts, a magical Sorting Hat sorts the first years among four school Houses (Gryffindor, Hufflepuff, Ravenclaw, and Slytherin) that best suit their personalities and talents. Draco joins Slytherin, known for producing dark wizards, while Harry, Ron, and Hermione are sorted into Gryffindor. Harry''s broomstick flying ability earns him a place on the Gryffindor Quidditch team as the Seeker. Harry comes to dislike Potions master Severus Snape, who favours Slytherin while seeking to fail Harry. Malfoy tricks Harry and Ron into risking expulsion by leaving their common room after curfew. Hermione, unable to stop them, tags along. Realising Malfoy''s ruse, they hide in a forbidden corridor and discover a gigantic three-headed dog guarding a trapdoor. Harry and Ron later save Hermione from a troll during a Halloween celebration and the three become best friends. Coupled with Snape''s recent leg injury and suspicious behaviour, Harry, Ron, and Hermione believe he is attempting to enter the trapdoor. During Harry''s first Quidditch match, his broomstick attempts to buck him off. Snape''s strange behaviour during the match convinces Hermione he jinxed Harry''s broom. Harry receives an anonymous Christmas gift – his father''s invisibility cloak. Using it to explore the school, he discovers the Mirror of Erised, which shows what the viewer most desires. Harry sees his parents. A newspaper report later describes an attempted robbery at the same vault in Gringotts from which Hagrid retrieved an item for Hogwarts'' headmaster Albus Dumbledore on the same day. The object beneath the trapdoor is the philosopher''s stone, which grants its user immortality and the ability to turn any metal into pure gold. Firenze, a forest centaur, warns Harry that Voldemort is plotting to steal the stone to restore his body. When Dumbledore is lured from Hogwarts under false pretences, Harry, Hermione, and Ron fear the theft is imminent and descend through the trapdoor. Various obstacles force Ron and Hermione to remain behind while Harry proceeds. Harry encounters Professor Quirrell, the Defence Against the Dark Arts teacher. He jinxed Harry''s broom and let the troll into the school; Snape was protecting Harry. Voldemort, whose face has regrown on the back of Quirrell''s head, is revealed to be the professor''s secret master. Harry is forced to stand before the Mirror of Erised. It recognises Harry''s lack of greed for the stone and deposits it into his pocket. Quirrell attempts to seize the stone, but his flesh burns upon contact with Harry. Harry''s scar begins hurting, and he passes out. Harry awakens in the school''s infirmary. He survived Voldemort because his mother sacrificing her life for him left a magical protective charm. Quirrell''s hatred and greed caused him to burn upon contact with Harry; Voldemort abandoned him to die. Dumbledore reveals he sent Harry the invisibility cloak, while the philosopher''s stone, that had been keeping the long-lived Nicolas Flamel alive, has been destroyed with Flamel''s consent to prevent its being stolen. During the school''s year-end feast, Gryffindor is awarded the House Cup. Harry returns to the Dursleys at Privet Drive until the next school year.', '1997-07-30'),
-  ('Harry Potter and the Chamber of Secrets','harry-potter-and-the-chamber-of-secrets', 'JK. Rowling', '2345678901234', true, 'https://images.ctfassets.net/usf1vwtuqyxm/6VuPUG4wPtmg3xvqwm6ZSn/2465365bae53ac09562f7bcea0b75cb3/English_Harry_Potter_2_Epub_9781781100226.jpg?w=914&q=70&fm=webp', '', '1998-07-02'),
-  ('Harry Potter and the Prisoner of Azkaban','harry-potter-and-the-prisoner-of-azkaban', 'JK. Rowling', '2345678901234', true, 'https://images.ctfassets.net/usf1vwtuqyxm/24YWmI4UcyoMwj7wdKrEcL/374de1941927db12bd844fb197eab11f/English_Harry_Potter_3_Epub_9781781100233.jpg?w=914&q=70&fm=webp', '', '1999-07-08'),
-  ('Harry Potter and the Goblet of Fire','harry-potter-and-the-goblet-of-fire', 'JK. Rowling', '2345678901234', true, 'https://images.ctfassets.net/usf1vwtuqyxm/3d9kpFpwHyjACq8H3EU6ra/85673f9e660407e5e4481b1825968043/English_Harry_Potter_4_Epub_9781781105672.jpg?w=914&q=70&fm=webp', '', '2000-07-08'),
-  ('Harry Potter and the Order of The Phoenix','harry-potter-and-the-order-of-the-phoenix', 'JK. Rowling', '2345678901234', true, 'https://images.ctfassets.net/usf1vwtuqyxm/29op5HEVpvrKK2JKYCsFiO/5b939002fe3611b3f77659df83a76551/English_Harry_Potter_5_Epub_9781781100240.jpg?w=914&q=70&fm=webp', '', '2003-07-21'),
-  ('Harry Potter and the Half Blood Prince','harry-potter-and-the-half-blood-prince', 'JK. Rowling', '2345678901234', true, 'https://images.ctfassets.net/usf1vwtuqyxm/35KbpLHvQvQtBBKs0vKErL/43985bc9e5bea863ccf9cc9561b62827/English_Harry_Potter_6_Epub_9781781100257.jpg?w=914&q=70&fm=webp', '', '2005-07-16'),
-  ('Harry Potter and the Deathly Hallows','harry-potter-and-the-deathly-hallows','JK. Rowling', '2345678901234', true, 'https://images.ctfassets.net/usf1vwtuqyxm/6S51pK7uwnyhkS9Io9DsAn/320c162c5150f853b8d8568c4715dcef/English_Harry_Potter_7_Epub_9781781100264.jpg?w=914&q=70&fm=webp', '', '2007-07-21')
+INSERT INTO books (
+  title, genre, fileUrl, slug, author, isbn, 
+  is_borrowed, imageUrl, synopsis, 
+  published_date
+) 
+VALUES 
+  (
+    'Harry Potter and the Philosopher Stone', 
+    'Fantasy,Mystery,Thriller',
+    'https://res.cloudinary.com/dhrs3yldr/raw/upload/v1698915903/Harry_Potter_and_the_Sorcerer_39_s_Stone_Harry_Potter_1_iozyii.epub', 
+    'harry-potter-and-the-philosopher-stone', 
+    'JK. Rowling', '1234567890123', 
+    false, 'https://images.ctfassets.net/usf1vwtuqyxm/2DCs73x6P8seNobQ9zBSbO/1a5dfd6ed5fc0ed9545370470fc3d74c/English_Harry_Potter_1_Epub_9781781100219.jpg?w=914&q=70&fm=webp', 
+    'On his eleventh birthday, Harry Potter learns he is a wizard and receives an acceptance letter to Hogwarts School of Witchcraft and Wizardry. He discovers his parents were wizards and that he survived an attack by the dark wizard Voldemort as a baby. At Hogwarts, he makes friends with Ron Weasley and Hermione Granger, gets sorted into Gryffindor, and learns about the Sorcerer''s Stone. The stone, hidden in the school, can grant immortality. Harry, Ron, and Hermione suspect someone is trying to steal it, leading to a showdown with Professor Quirrell and the revelation of Voldemort''s return.', 
+    '1997-07-30'
+  ), 
+  (
+    'Harry Potter and the Chamber of Secrets',
+    'Fantasy,Mystery,Thriller', 
+    'https://res.cloudinary.com/dhrs3yldr/raw/upload/v1698917737/Harry_Potter_and_the_Chamber_of_Secrets_Harry_Potter_2_fbfbbu.epub', 
+    'harry-potter-and-the-chamber-of-secrets', 
+    'JK. Rowling', '2345678901234', 
+    true, 'https://images.ctfassets.net/usf1vwtuqyxm/6VuPUG4wPtmg3xvqwm6ZSn/2465365bae53ac09562f7bcea0b75cb3/English_Harry_Potter_2_Epub_9781781100226.jpg?w=914&q=70&fm=webp', 
+    'In the second year at Hogwarts, Harry Potter returns after a summer at the Dursleys. Mysterious events unfold as students and even Filch''s cat are found petrified. Harry, Ron, and Hermione discover the Chamber of Secrets has been opened. Harry finds Tom Riddle''s enchanted diary, revealing the chamber''s history. The trio suspects Draco Malfoy and Dobby, the house-elf. Harry enters the chamber, defeats a basilisk, and destroys the diary to save Ginny Weasley. The school year ends with Gryffindor winning the House Cup.', 
+    '1998-07-02'
+  ), 
+  (
+    'Harry Potter and the Prisoner of Azkaban',
+    'Fantasy,Mystery,Thriller', 
+    'https://res.cloudinary.com/dhrs3yldr/raw/upload/v1698917737/Harry_Potter_and_the_Prisoner_of_Azkaban_Harry_Potter_3_qlctwo.epub', 
+    'harry-potter-and-the-prisoner-of-azkaban', 
+    'JK. Rowling', '2345678901234', 
+    true, 'https://images.ctfassets.net/usf1vwtuqyxm/24YWmI4UcyoMwj7wdKrEcL/374de1941927db12bd844fb197eab11f/English_Harry_Potter_3_Epub_9781781100233.jpg?w=914&q=70&fm=webp', 
+    'In his third year at Hogwarts, Harry learns that Sirius Black, an escaped prisoner, is believed to be a threat. Dementors guard the school, causing Harry to pass out. He learns the truth about Sirius, who is his godfather and not a traitor. With the help of a time-turner, they save Sirius and Buckbeak, the hippogriff.', 
+    '1999-07-08'
+  ), 
+  (
+    'Harry Potter and the Goblet of Fire',
+    'Fantasy,Mystery,Thriller', 
+    'https://res.cloudinary.com/dhrs3yldr/raw/upload/v1698917736/Harry_Potter_and_the_Goblet_of_Fire_Harry_Potter_4_urvyok.epub', 
+    'harry-potter-and-the-goblet-of-fire', 
+    'JK. Rowling', '2345678901234', 
+    true, 'https://images.ctfassets.net/usf1vwtuqyxm/3d9kpFpwHyjACq8H3EU6ra/85673f9e660407e5e4481b1825968043/English_Harry_Potter_4_Epub_9781781105672.jpg?w=914&q=70&fm=webp', 
+    'Harry is unexpectedly entered into the Triwizard Tournament, a dangerous competition. He faces various challenges, including a Hungarian Horntail dragon and the underwater task. Ultimately, he encounters Voldemort and his Death Eaters, leading to a rebirth of the Dark Lord.', 
+    '2000-07-08'
+  ), 
+  (
+    'Harry Potter and the Order of The Phoenix',
+    'Fantasy,Mystery,Thriller', 
+    'https://res.cloudinary.com/dhrs3yldr/raw/upload/v1698917736/Harry_Potter_and_the_Order_of_the_Phoenix_Harry_Potter_5_kkyquh.epub', 
+    'harry-potter-and-the-order-of-the-phoenix', 
+    'JK. Rowling', '2345678901234', 
+    true, 'https://images.ctfassets.net/usf1vwtuqyxm/29op5HEVpvrKK2JKYCsFiO/5b939002fe3611b3f77659df83a76551/English_Harry_Potter_5_Epub_9781781100240.jpg?w=914&q=70&fm=webp', 
+    'Harry returns to Hogwarts for his fifth year, facing challenges and isolation. He forms Dumbledore''s Army to resist the oppressive regime of Dolores Umbridge. The climactic battle at the Department of Mysteries reveals a prophecy about Harry and Voldemort.', 
+    '2003-07-21'
+  ), 
+  (
+    'Harry Potter and the Half Blood Prince',
+    'Fantasy,Mystery,Thriller', 
+    'https://res.cloudinary.com/dhrs3yldr/raw/upload/v1698917736/Harry_Potter_and_the_Half-Blood_Prince_Harry_Potter_6_wqetuk.epub', 
+    'harry-potter-and-the-half-blood-prince', 
+    'JK. Rowling', '2345678901234', 
+    true, 'https://images.ctfassets.net/usf1vwtuqyxm/35KbpLHvQvQtBBKs0vKErL/43985bc9e5bea863ccf9cc9561b62827/English_Harry_Potter_6_Epub_9781781100257.jpg?w=914&q=70&fm=webp', 
+    'Harry learns about Voldemort''s Horcruxes and discovers the identity of the Half-Blood Prince, Severus Snape. Tragedy strikes as Dumbledore is killed by Snape, and the Death Eaters begin their rise to power.', 
+    '2005-07-16'
+  ), 
+  (
+    'Harry Potter and the Deathly Hallows',
+    'Fantasy,Mystery,Thriller', 
+    'https://res.cloudinary.com/dhrs3yldr/raw/upload/v1698917735/Harry_Potter_and_the_Deathly_Hallows_Harry_Potter_7_ibzvpx.epub', 
+    'harry-potter-and-the-deathly-hallows', 
+    'JK. Rowling', '2345678901234', 
+    true, 'https://images.ctfassets.net/usf1vwtuqyxm/6S51pK7uwnyhkS9Io9DsAn/320c162c5150f853b8d8568c4715dcef/English_Harry_Potter_7_Epub_9781781100264.jpg?w=914&q=70&fm=webp', 
+    'In the final book, Harry, Ron, and Hermione go on a quest to find and destroy Horcruxes to weaken Voldemort. The Deathly Hallows, three magical objects, play a crucial role. The story ends with the climactic Battle of Hogwarts, the final defeat of Voldemort, and the epilogue showing the characters'' futures.', 
+    '2007-07-21'
+  )
